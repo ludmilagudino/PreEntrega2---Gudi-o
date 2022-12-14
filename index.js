c2d27f5b368a46a23dd2a9b335b93c1e3df328e1
ludmilagudino <ludmitrini@gmail.com>
2022-12-14 00:44:38 -0300

    Metodos de busqueda y filtrado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 class Producto {
     constructor(nombre, precio, esencia) {
         this.nombre = nombre;
-        this.precio = precio;
+        this.precio = parseInt(precio) ;
         this.esencia = esencia;
         this.disponible = true;
     }
@@ -22,7 +22,7 @@ var cargaProductos = prompt("Ingrese nombre del producto o 'fin' para terminar l
 
 while (cargaProductos != "fin"){
     let nombre = cargaProductos;
-    let precio = prompt("Ingrese el precio del producto: ");
+    let precio = parseInt(prompt("Ingrese el precio del producto: "));
     let esencia = prompt("Ingrese esencia de la vela: ");
 
     velas.push(new Producto(nombre, precio, esencia));
@@ -32,6 +32,47 @@ while (cargaProductos != "fin"){
 }
 //Array con productos cargados
 console.log(velas);
+
+
+//FILTER para filtrar por precio minimo
+
+let precioMin = parseInt(prompt("Ingrese el precio minimo"));
+
+let filtrados = velas.filter((producto) => producto.precio > precioMin); 
+
+filtrados.forEach((producto) => {
+    let mensaje = `
+    Nombre: ${producto.nombre}
+    Precio: $${producto.precio}
+    Esencia: ${producto.esencia}
+`;
+
+    alert(mensaje);
+});
+
+//FILTER para encontrar producto con la esencia ingresada
+
+let nombreEsencia = prompt("Ingrese el nombre de la esencia que desea buscar: ");
+let encontrado = velas.find(item => item.esencia === nombreEsencia);
+
+let mensaje2 = `
+    Nombre: ${encontrado.nombre}
+    Precio: $${encontrado.precio}
+    Esencia: ${encontrado.esencia}
+`;
+
+alert(mensaje2);
+
+
+let nombreDisp = prompt("Ingrese el nombre del producto a consultar disponibilidad: ");
+
+if(velas.some(item => item.nombre === nombreDisp)){
+    alert("El producto se encuentra disponible");
+}else{
+    alert("El producto no se encuentra disponible");
+}
+
+
 
 
 
